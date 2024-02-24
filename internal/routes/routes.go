61e15bf1d6a42b94f7d1c037f9eec9aad37e5648
Zachary Kent <ztkent@gmail.com>
2024-02-23 23:59:06 -0500

    prevent empty download file name

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -71,7 +71,10 @@ func (a *Augur) Download() http.HandlerFunc {
 			return
 		}
 		r.ParseForm()
-		appName := strings.ReplaceAll(r.Form.Get("appName"), " ", "_")
+		appName := "prompt"
+		if r.Form.Get("appName") != "" {
+			appName = strings.ReplaceAll(r.Form.Get("appName"), " ", "_")
+		}
 		w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=%s.md", appName))
 		w.Header().Set("Content-Type", "application/octet-stream")
 		http.ServeFile(w, r, fmt.Sprintf("temp/response_%s.md", uuid))
